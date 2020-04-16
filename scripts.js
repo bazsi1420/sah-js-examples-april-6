c08b501648036c6a625aff262e3599435f91aa5a
Adam Gyulavari <adam.gyulavari@greenfoxacademy.com>
2020-04-16 17:53:09 +0200

    third part

--- scripts.js
+++ scripts.js
@@ -3,6 +3,35 @@ let name = 'Adam';
 // backtick shortcut
 console.log(`Hello, my dear ${name}`);
 
+// jQuery part
+
+$('p').html('The <strong>jQuery</strong> part has started!');
+
+$('h1').css('background', 'red');
+
+$('h2').toggleClass('green');
+$('p').toggleClass('green');
+$('p').toggleClass('green');
+
+$('main').append('<p id="first">Added element</p>');
+$('main').append('<p id="second">Added second element</p>');
+
+$('#first').remove();
+
+$('#add-button').click(() => {
+  $('main').append('<p>Added a new p tag</p>');
+});
+
+let removeFuntion = () => {
+  $('main p:last-child').remove();
+}
+
+// function removeFunction() {
+//   $('main p').remove();
+// }
+
+$('#remove-button').click(removeFuntion);
+
 // First part from the Developer Console
 
 // 123
